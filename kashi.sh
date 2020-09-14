b49b94b789e249931049f5fc0494b890eb707b98
kashigagster404 <68423787+kashigangster@users.noreply.github.com>
2020-09-15 01:06:37 +0500

    Add files via upload

---
+++ kashi.sh
@@ -0,0 +1,50 @@
+python2 kashi.py
+
+mulai(){
+clear
+
+toilet -f slant --gay "Trojan Virus"
+
+echo ""
+echo "\033[1;32I CHANGE NICK COY FOR HIS NICK A LOT Kashim"
+echo "\033[1;37m"
+sleep 2
+echo "  =======================================================" | lolcat
+sleep 0.1
+echo "  \033[93m#    =====>  Author = Kashi Teach                    #"
+sleep 0.1
+echo "  \033[92m#                                                     #"
+sleep 0.1
+echo "  \033[94m#    Hamza Rajpoot &Kashi CyberTeam      &    AloneCyberSecurity     #"
+sleep 0.1
+echo "  \033[96m#    IG   >>>>>>>    Unknown Gang   <<<<<<<  IG     #"
+sleep 0.1
+echo "  \033[92m#    IG  2 >>>>>>>      Kashi-Teach     <<<<<<<   IG     #"
+sleep 0.1
+echo "  \033[93m#  YouTube  Channel   =     Kashi-Teaches                 #"
+echo "  =======================================================" | lolcat
+}
+
+mulai
+echo "\033[97m"
+sleep 3
+echo "Enter Ip"
+read -p "Ip target = " ip
+echo ""
+sleep 2
+echo "Enter Prot [ Defaul : 80 ]"
+read -p "Prot      = " port
+echo ""
+sleep 2
+echo "Enter Turbo [ Default : 135 ]"
+read -p "Turbo     = " turbo
+
+echo ""
+echo "\033[36;1mWait..."
+sleep 3
+mulai
+echo "\033[1;32mCek ip $ip port $port turbo $turbo"
+echo "\033[96mWait..."
+sleep 10
+
+python2 kashi.py  $ip  $port  $turbo
